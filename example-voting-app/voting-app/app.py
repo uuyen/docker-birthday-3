b6934137dcddc3d27365e2d48d401b1cfdde8f66
Mano Marks <mano.marks@gmail.com>
2016-03-16 17:44:07 -0700

    Added the Running your app section and reworked the submission guidelines

--- example-voting-app/voting-app/app.py
+++ example-voting-app/voting-app/app.py
@@ -8,8 +8,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "One")
-option_b = os.getenv('OPTION_B', "Two")
+option_a = os.getenv('OPTION_A', "Python")
+option_b = os.getenv('OPTION_B', "Javascript")
 
 hostname = socket.gethostname()
 
